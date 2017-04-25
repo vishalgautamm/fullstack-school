176d356c9eb4a40a3b08f6eb07f1662b03d2f00c
vishalgautamm <vishal.g@live.ca>
2017-04-25 00:14:28 -0400

    Added router links to courses component

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,18 +1,19 @@
 import React, {Component} from 'react';
+import NavLink from './NavLink';
 
 class Courses extends Component {
   render() {
     return (
       <div className="main-content courses">
         <div className="course-header group">
-          <h2>Courses</h2> 
+          <h2>Courses</h2>
           <ul className="course-nav">
-            <li><a href="/courses/html">HTML</a></li>
-            <li><a href="/courses/css">CSS</a></li>
-            <li><a href="/courses/javascript">JavaScript</a></li>
+            <li><NavLink to="/courses/html">HTML</NavLink></li>
+            <li><NavLink to="/courses/css">CSS</NavLink></li>
+            <li><NavLink to="/courses/javascript">JavaScript</NavLink></li>
           </ul>
         </div>
-        
+        { this.props.children }
       </div>
     );
   }
--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, IndexRedirect } from 'react-router';
 
 // components
 import App from './components/App';
@@ -8,6 +8,10 @@ import About from './components/About';
 import Courses from './components/Courses';
 import Teachers from './components/Teachers';
 
+import HTML from './components/courses/HTML';
+import CSS from './components/courses/CSS';
+import Javascript from './components/courses/Javascript';
+
 // Routes
 const Routes = (
   <Router history={browserHistory} >
@@ -15,7 +19,13 @@ const Routes = (
       <Route path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/teachers" component={Teachers} />
-      <Route path="/courses" component={Courses} />
+
+      <Route path="/courses" component={Courses}>
+        <IndexRedirect to="Javascript" />
+        <Route path="html" component={HTML} />
+        <Route path="css" component={CSS} />
+        <Route path="javascript" component={Javascript} />
+      </Route>
     </Route>
   </Router>
 );
