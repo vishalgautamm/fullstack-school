b4d49ba40a4fb20ac439b3e7000d4bcb2194b5eb
vishalgautamm <vishal.g@live.ca>
2017-04-24 22:52:28 -0400

    Organized routes into a different component

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
+//Libs
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
 
 //CSS
 import './css/style.css';
 
-// components
-import App from './components/App';
-import Home from './components/Home';
-import About from './components/About';
-import Courses from './components/Courses';
-import Teachers from './components/Teachers';
+// Routes
+import routes from './router';
 
+// Render
 const mountNode = document.getElementById('root');
-render((
-  <Router history={hashHistory} >
-    <Route component={App}>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/teachers" component={Teachers} />
-      <Route path="/courses" component={Courses} />
-    </Route>
-  </Router>
-), mountNode);
+render(
+  routes,
+  mountNode);
---
+++ src/router.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Router, Route, hashHistory } from 'react-router';
+
+// components
+import App from './components/App';
+import Home from './components/Home';
+import About from './components/About';
+import Courses from './components/Courses';
+import Teachers from './components/Teachers';
+
+// Routes
+const Routes = (
+  <Router history={hashHistory} >
+    <Route component={App}>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/teachers" component={Teachers} />
+      <Route path="/courses" component={Courses} />
+    </Route>
+  </Router>
+);
+
+export default Routes;
